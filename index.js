a00dabe069155908cd4a45406b8ab2314970443d
gomuningen2004 <govardhan.personal.new@gmail.com>
2024-06-12 22:54:23 +0530

    added a post

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-
-
-// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 const year = new Date().getFullYear();
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    currentYear : year
+  res.render("index", {
+    currentYear: year
   });
 });
 
 app.get("/about", (req, res) => {
-  // res.sendFile((__dirname + "/public/static/contact.html"));
-  res.render("about.ejs", {
-    currentYear : year
+  res.render("about", {
+    currentYear: year
   });
 });
 
